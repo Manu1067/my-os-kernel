69f0f46e783c2dbeb526b4839514ce4751006594
Manu1067 <vmanushree2006@gmail.com>
2026-04-06 22:10:25 +0530

    Fixing Error

--- boot/boot.asm
+++ boot/boot.asm
@@ -6,29 +6,16 @@ start:
 
 print:
     lodsb
-    cmp al, 0
-    je done
+    or al, al
+    jz done
     mov ah, 0x0e
     int 0x10
     jmp print
 
 done:
-    call load_kernel
-    jmp 0x0000:0x1000
+    jmp $
 
-; -------- LOAD KERNEL --------
-load_kernel:
-    mov ah, 0x02        ; read disk
-    mov al, 10          ; read 10 sectors (IMPORTANT)
-    mov ch, 0
-    mov cl, 2           ; start from sector 2
-    mov dh, 0
-    mov dl, 0x80
-    mov bx, 0x1000      ; load address
-    int 0x13
-    ret
-
-message db "HELLO FROM BOOTLOADER", 0
+message db 'Booting OS...', 0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
