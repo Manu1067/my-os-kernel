535701176d6af8e66472301b268a4fed58d97f15
Sayantika Das <sayantikadas@Sayantikas-MacBook-Air.local>
2026-04-06 21:38:16 +0530

    Loading kernel into memory

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,8 +4,17 @@ void main() {
     video[0] = 'H';
     video[1] = 0x07;
 
-    video[2] = 'i';
+    video[2] = 'E';
     video[3] = 0x07;
+
+    video[4] = 'L';
+    video[5] = 0x07;
+
+    video[6] = 'L';
+    video[7] = 0x07;
+
+    video[8] = 'O';
+    video[9] = 0x07;
 
     while(1);
 }
