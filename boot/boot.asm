ab1d883c2706c5d301d5c3f446ccbfe359a655e0
Manu1067 <vmanushree2006@gmail.com>
2026-04-06 23:33:36 +0530

    Removing garbage kernel output fixes

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,21 +1,61 @@
 [org 0x7c00]
 KERNEL_OFFSET equ 0x1000 
 
-mov [BOOT_DRIVE], dl 
-mov bp, 0x9000
-mov sp, bp
+    mov [BOOT_DRIVE], dl
+    mov bp, 0x9000
+    mov sp, bp
 
-call load_kernel
-call switch_to_pm ; This switches to 32-bit mode and jumps to BEGIN_PM
-jmp $
+    call load_kernel
+    call switch_to_pm
+    jmp $
 
-%include "disk.asm"
-%include "gdt.asm"
-%include "switch.asm"
+[bits 16]
+load_kernel:
+    mov bx, KERNEL_OFFSET 
+    mov dh, 15            ; Read 15 sectors
+    mov dl, [BOOT_DRIVE]
+    mov ah, 0x02
+    mov al, dh
+    mov ch, 0x00
+    mov dh, 0x00
+    mov cl, 0x02          ; Kernel starts at sector 2
+    int 0x13
+    ret
+
+; GDT Definition
+gdt_start:
+    dq 0x0
+gdt_code:
+    dw 0xffff, 0x0, 0x9a00, 0x00cf
+gdt_data:
+    dw 0xffff, 0x0, 0x9200, 0x00cf
+gdt_end:
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+[bits 16]
+switch_to_pm:
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:init_pm
 
 [bits 32]
-BEGIN_PM:
-    call KERNEL_OFFSET ; Jump to the C kernel
+init_pm:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ebp, 0x90000
+    mov esp, ebp
+    call KERNEL_OFFSET
     jmp $
 
 BOOT_DRIVE db 0
