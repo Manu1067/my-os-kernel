c42807c6fccb09bf670c26893bc15f18578e0211
Sayantika Das <sayantikadas@Sayantikas-MacBook-Air.local>
2026-04-06 22:06:58 +0530

    Fixing error

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,20 +1,12 @@
 void main() {
     char *video = (char*) 0xb8000;
 
-    video[0] = 'H';
-    video[1] = 0x07;
-
-    video[2] = 'E';
-    video[3] = 0x07;
-
-    video[4] = 'L';
-    video[5] = 0x07;
-
-    video[6] = 'L';
-    video[7] = 0x07;
-
-    video[8] = 'O';
-    video[9] = 0x07;
+    char *msg = "Hello World from Kernel!";
+    
+    for (int i = 0; msg[i] != '\0'; i++) {
+        video[i * 2] = msg[i];
+        video[i * 2 + 1] = 0x0A;  // Green color
+    }
 
     while(1);
 }
