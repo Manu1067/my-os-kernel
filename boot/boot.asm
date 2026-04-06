b44cd1fc5cc376cfe143da45c1e4402994a127c5
Manu1067 <vmanushree2006@gmail.com>
2026-04-07 00:05:25 +0530

    making changes to  bootloader

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,63 +1,49 @@
-[org 0x7c00]
-KERNEL_OFFSET equ 0x1000 
+[BITS 16]
+[ORG 0x7c00]
 
-    mov [BOOT_DRIVE], dl
-    mov bp, 0x9000
-    mov sp, bp
+start:
+    mov si, msg1
+    call print_string
 
-    call load_kernel
-    call switch_to_pm
-    jmp $
+    call new_line
 
-[bits 16]
-load_kernel:
-    mov bx, KERNEL_OFFSET 
-    mov dh, 15            ; Read 15 sectors
-    mov dl, [BOOT_DRIVE]
+    ; load kernel from disk (sector 2)
     mov ah, 0x02
-    mov al, dh
-    mov ch, 0x00
-    mov dh, 0x00
-    mov cl, 0x02          ; Kernel starts at sector 2
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0x80
+    mov bx, 0x1000
     int 0x13
+
+    ; jump to kernel
+    jmp 0x0000:0x1000
+
+
+; -------- PRINT FUNCTION --------
+print_string:
+    lodsb
+    or al, al
+    jz done
+    mov ah, 0x0e
+    int 0x10
+    jmp print_string
+done:
     ret
 
-; GDT Definition
-gdt_start:
-    dq 0x0
-gdt_code:
-    dw 0xffff, 0x0, 0x9a00, 0x00cf
-gdt_data:
-    dw 0xffff, 0x0, 0x9200, 0x00cf
-gdt_end:
-gdt_descriptor:
-    dw gdt_end - gdt_start - 1
-    dd gdt_start
-CODE_SEG equ gdt_code - gdt_start
-DATA_SEG equ gdt_data - gdt_start
 
-[bits 16]
-switch_to_pm:
-    cli
-    lgdt [gdt_descriptor]
-    mov eax, cr0
-    or eax, 0x1
-    mov cr0, eax
-    jmp CODE_SEG:init_pm
+; -------- NEW LINE --------
+new_line:
+    mov ah, 0x0e
+    mov al, 0x0d
+    int 0x10
+    mov al, 0x0a
+    int 0x10
+    ret
 
-[bits 32]
-init_pm:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov ss, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ebp, 0x90000
-    mov esp, ebp
-    call KERNEL_OFFSET
-    jmp $
 
-BOOT_DRIVE db 0
+msg1 db "Hello from Bootloader", 0
+
 times 510-($-$$) db 0
 dw 0xaa55
