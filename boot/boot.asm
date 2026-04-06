8555432d32b8394d9a70664432e95101dc6c103f
Manu1067 <vmanushree2006@gmail.com>
2026-04-06 19:32:59 +0530

    Printing the message instead of H

--- boot/boot.asm
+++ boot/boot.asm
@@ -2,11 +2,20 @@
 [ORG 0x7c00]
 
 start:
-    mov ah, 0x0e
-    mov al, "Hello World"
-    int 0x10
+    mov si, message
 
+print:
+    lodsb
+    cmp al, 0
+    je done
+    mov ah, 0x0e
+    int 0x10
+    jmp print
+
+done:
     jmp $
 
+message db "HELLO FROM BOOTLOADER", 0
+
 times 510 - ($ - $$) db 0
-dw 0xaa55git
+dw 0xaa55
